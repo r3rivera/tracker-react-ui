8774f87cb3ea5d585f65c0bd5ed4e22792b38ec2
R2 Rivera <r.ryan.rivera@gmail.com>
2025-01-14 16:14:45 -0600

    Added googlrmap api

--- src/components/main/MapTracker.jsx
+++ src/components/main/MapTracker.jsx
@@ -1,9 +1,37 @@
+import { useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+import './MapTracker.scss';
+
 function MapTracker(){
 
+    useEffect(() => {
+
+        const loader = new Loader({apiKey: "",version: "weekly"});
+        loader.importLibrary('maps')
+              .then(({Map}) => {
+                new Map(document.getElementById("R2Map"),{
+                    center: { lat: 33.207, lng: -96.701 },
+                    zoom: 13,
+                  });
+              })
+              .catch((e) => {
+                console.log(`Error loading maps: ${e}`);
+            });
+        
+        return () => {
+            console.log("Map component clean up!");
+            const srcUrl = document.querySelector("div#R2Map");
+            if (srcUrl){
+                //document.body.removeChild(srcUrl);
+            }
+        };
+        
+
+    }, []);
+   
+
     return (
-
-    <h1 className="text-3xl text-black pb-6">Maps Tracker</h1>
-
+    <div id="R2Map"></div>
     );
 }
 export default MapTracker;
